d78fca9d9211fd3f501a57bb619cd5bfb9b5c9fa
Lise Klaksvik <lisenklaksvik@gmail.com>
2024-02-20 13:42:37 +0000

    Refactored cli.py into separate .py files, within directory /ser

--- bin/cli.py
+++ bin/cli.py
@@ -19,12 +19,18 @@ def train(
     name: str = typer.Option(
         ..., "-n", "--name", help="Name of experiment to save under."
     ),
+    epochs: int = typer.Option(
+        ...,"-e", "--epochs", help="Number of epochs for training."
+    ),
+    batch_size: int = typer.Option(
+        ...,"-bs","--batch_size", help="Batch size for training."
+    ),
+    learning_rate: float = typer.Option(
+        ...,"lr","--learning_rate", help="Learning rate for training"
+    )
 ):
     print(f"Running experiment {name}")
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    epochs = 2
-    batch_size = 1000
-    learning_rate = 0.01
 
     # save the parameters!
 
