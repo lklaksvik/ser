48b2f721aeaed42ffceb13772b2bb2e7391e10a8
Lise Klaksvik <lisenklaksvik@gmail.com>
2024-02-22 15:29:31 +0000

    finished day 2 solutions

---
+++ ser/utils.py
@@ -0,0 +1,39 @@
+import json
+import torch
+import os
+import dataclasses
+
+result_dir = "results"
+
+
+def make_hyperparameter_dict(parameters):
+    hyperparameter_dict = dataclasses.asdict(parameters)
+    return hyperparameter_dict
+
+
+def write_json(experiment_name, data):
+
+    save_dir = os.path.join(result_dir,experiment_name)
+
+    # create results folder, if does not exist
+    os.makedirs(result_dir, exist_ok=True)
+    # create save folder within results folder, if does not exist
+    os.makedirs(save_dir, exist_ok=True)
+
+    hyperparameter_dict = make_hyperparameter_dict(data)
+
+    with open(os.path.join(save_dir, f"{experiment_name}_json"), 'w') as f:
+        json.dump(hyperparameter_dict, f)
+
+
+def save_model(experiment_name,trained_model):
+
+    save_dir = os.path.join(result_dir,experiment_name)
+
+    # create results folder, if does not exist
+    os.makedirs(result_dir, exist_ok=True)
+    # create save folder within results folder, if does not exist
+    os.makedirs(save_dir, exist_ok=True)
+
+    torch.save(trained_model.state_dict(), os.path.join(save_dir, f"{experiment_name}_model.pt"))
+
